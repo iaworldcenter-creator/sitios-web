e93775c6afef446f33a122abc3a38bcc6bb1b065
iaworldcenter-creator <iaworldcenter@gmail.com>
2026-08-22 02:54:45 -0600

    chore: actualizar submodule bazar-viamx-nfl.gdl depurado y reiniciado

--- bazar-viamx-nfl.gdl/Arrancar_Demonio_Local.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run """C:\Users\nflgd\AppData\Local\Programs\Python\Python312\python.exe"" ""d:\Downloads\Proyecto Web\viamx_universal_crawler.py""", 0, False
--- bazar-viamx-nfl.gdl/read_log_last.py
+++
@@ -1,7 +0,0 @@
-with open(r"d:\Downloads\Proyecto Web\log_operativo.txt", 'r', encoding='utf-8', errors='ignore') as f:
-    lines = f.readlines()
-
-print(f"Total lines in log: {len(lines)}")
-print("--- Last 100 lines ---")
-for line in lines[-100:]:
-    print(line, end='')
